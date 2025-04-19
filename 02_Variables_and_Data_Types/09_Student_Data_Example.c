8983963588b03f470e2e0dbd7000e20a824e39e1
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-19 23:08:35 +0600

    uploading two practice example

---
+++ 02_Variables_and_Data_Types/09_Student_Data_Example.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    // Student Data
+    int studentID = 220320888;
+    int studentAge = 23;
+    float studentFee = 27362.383;
+    char studentGrade = 'B';
+
+    // Printing value
+    printf("Student ID: %d\n", studentID);
+    printf("Student Age is: %d\n", studentAge);
+    printf("Student Fee is: %f\n", studentFee);
+    printf("Student Grade is: %c", studentGrade);
+    return 0;
+}
---
+++ 02_Variables_and_Data_Types/10_Area_of_Rectangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    float height = 36.2;
+    float width = 3628;
+    float area;
+// Calculating area of rectangle
+area = height * width;
+// Printing all variable
+printf("Height is: %f\n", height);
+printf("width is: %f\n", width);
+printf("Area of this rectangle is: %f", area);
+    return 0;
+}
