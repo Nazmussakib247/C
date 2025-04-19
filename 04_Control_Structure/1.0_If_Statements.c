48d3a4670fbd860e819b630602199ba1f85e5a5c
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-20 01:58:04 +0600

    if statements basic

---
+++ 04_Control_Structure/1.0_If_Statements.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    // Comparing two values using if statement
+    if (20 > 18) {
+        printf("20 is greater than 18");
+    }
+    return 0;
+}
---
+++ 04_Control_Structure/1.1_Two_variable.c
@@ -0,0 +1,10 @@
+// Testing two variable using if statements
+# include <stdio.h>
+int main(){
+    int x = 2837383;
+    int y = 1378736;
+    if (x > y) {
+        printf("X is greater than Y");
+    }
+    return 0;
+}
