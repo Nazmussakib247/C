9fd00c1ef9671daea7935dfd5464b907a71108d6
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-20 00:23:04 +0600

    uploading add operation

---
+++ 03_Operator/01_Arithmatic_Operator/01_Add_Operation.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+int main(){
+    int myNumber = 100 + 382; //Using add operator in a line
+    printf("Total is: %d\n", myNumber);
+int sum1 = 284038 + 394743 ;
+int sum2 = sum1 + 38749347;
+int sum3 = sum2 + sum1;
+printf("sum1 is: %d\n", sum1);
+printf("Sum2 is: %d\n", sum2);
+printf("Sum3 is: %d", sum3);
+
+    return 0;
+}
