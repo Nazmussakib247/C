a5d5c479327506e0b3c8ffdb55013f8d5b93dd20
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-21 21:53:17 +0600

    adding else if

---
+++ 04_Control_Structure/3.0_Else_If.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int time = 20;
+    if (time < 10) {
+        printf("Good Morning");
+    } else if (time < 20)
+    {
+    printf("Good Day");
+    } else {
+        printf("Good evening");
+    }
+    
+    return 0;
+}
