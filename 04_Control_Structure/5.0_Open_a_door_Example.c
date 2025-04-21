99eaed1b6dc3922b562141a9d50a4830843d7dc0
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-21 21:53:56 +0600

    example practice(door)

---
+++ 04_Control_Structure/5.0_Open_a_door_Example.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+int main (){
+    int doorCode = 29392;
+    if (doorCode == 1000) {
+        printf("Door is open");
+    } else {
+        printf("Door is locked, cant open, Enter right code again");
+    }
+    return 0;
+}
