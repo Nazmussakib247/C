e377d7af220e4fa99b9b1d904abc53d5408ba308
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-23 11:26:57 +0600

    adding negative-positive finder

---
+++ 04_Control_Structure/06_Number_find_positive_or_negative.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int myNumber = 293;
+    if (myNumber > 0) {
+        printf("Number is Positive");
+    } else {
+        printf("Number is Negative");
+    }
+    return 0;
+}
