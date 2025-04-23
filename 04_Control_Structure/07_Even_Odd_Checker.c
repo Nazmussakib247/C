cd9f3352afeef39e851d3696308e2dfc23b06cda
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-23 11:27:10 +0600

    adding even odd checker

---
+++ 04_Control_Structure/07_Even_Odd_Checker.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    int myNumber = 29321;
+    if (myNumber % 2 == 0) {
+        printf("Number is even");
+    } else {
+        printf("Number is Odd");
+    }
+    return 0;
+}
