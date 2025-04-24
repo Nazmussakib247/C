7d5265478605fd0d205e80a5e7b2e8aa9aef59ca
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-24 09:45:43 +0600

    adding countdown practice c

---
+++ 04_Control_Structure/10_Countdown.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+    int countDown = 10;
+    while (countDown > 0)
+    {
+        printf("%d\n", countDown);
+        countDown --;
+    }
+    printf("Happy New Year\n");
+    return 0;
+}
