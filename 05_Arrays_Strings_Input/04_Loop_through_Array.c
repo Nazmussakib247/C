1daa952040c8cf19d309ea337fe33b4af3bda064
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-27 00:22:20 +0600

    adding loop through array

---
+++ 05_Arrays_Strings_Input/04_Loop_through_Array.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    int myArray[] = {5, 10, 15, 20, 25};
+    int i;
+    for (i = 0; i < 4; i++) {
+        printf("%d\n", myArray[i]); //Print all element of an array
+    }
+    return 0;
+}
