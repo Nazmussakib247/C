4cab973d820aafaa691d0e2841b13c3571ed5876
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-28 00:03:40 +0600

    adding set size of an array

---
+++ 05_Arrays_Strings_Input/05_Set_Size_Of_array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int myArray[4];
+    //adding element in the array
+    myArray[0] = 3763;
+    myArray[2] = 376392;
+    myArray[3] = 743632;
+    myArray[1] = 276326;
+    printf("%d\n", myArray[3]);
+    return 0;
+}
