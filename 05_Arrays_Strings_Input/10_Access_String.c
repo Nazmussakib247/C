f82e37cf638d9f8c2b0eea77521aa27c123919cd
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-29 00:08:07 +0600

    acess string adding

---
+++ 05_Arrays_Strings_Input/10_Access_String.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    char myString[] = "Hello World!";
+    printf("%c", myString[0] ); //Printing first character of the string---one character thats why format specifier %c
+    return 0;
+}
