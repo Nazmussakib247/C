bf372cec4a2b3f19d97d5f2f473159061f382ccc
Nazmus Sakib <nazmuss247@gmail.com>
2025-05-03 00:12:06 +0600

    adding speical character string

---
+++ 05_Arrays_Strings_Input/14_Speical_Character_String.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main (){
+    char txt[] = "We are the so-called \"Vikings\" from the north."; // Backlash will escape charater
+    char txt[] = "It\'s alright.";
+    char txt[] = "The character \\ is called backslash.";
+}
