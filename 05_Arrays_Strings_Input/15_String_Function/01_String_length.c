5bcfbde7ef42c11f9df6c327d43a9799102d2b63
Nazmus Sakib <nazmuss247@gmail.com>
2025-05-04 00:04:19 +0600

    adding string lenght

---
+++ 05_Arrays_Strings_Input/15_String_Function/01_String_length.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h> //String header file
+
+int main (){
+    char alphabet[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    printf("%d", strlen(alphabet)); //strlen() function return string size including space
+    return 0;
+}
