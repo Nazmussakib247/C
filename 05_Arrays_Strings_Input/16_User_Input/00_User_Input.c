7e262a637605889667cbc8381081f50e748d1cc9
Nazmus Sakib <nazmuss247@gmail.com>
2025-05-08 00:09:57 +0600

    Added user input

---
+++ 05_Arrays_Strings_Input/16_User_Input/00_User_Input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int myNumber;
+    printf("Enter Your Number: \n");
+    scanf("%d", &myNumber);
+
+    printf("Your Number is: %d", myNumber);
+    return 0;
+}
