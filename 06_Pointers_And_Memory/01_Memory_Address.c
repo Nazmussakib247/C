edaf0053d1408ff34101e76f15aa11d0c2cc2604
Nazmus Sakib <nazmuss247@gmail.com>
2025-05-10 00:15:25 +0600

    Memory adress added

---
+++ 06_Pointers_And_Memory/01_Memory_Address.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    int myAge = 49;
+    printf("%d", myAge); // Output of the values of variable
+    printf("%p", &myAge); //output of the memory address
+    return 0;
+}
