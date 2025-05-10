e6c4c3073b44a1d321618f538a0e3e30c2b9d749
Nazmus Sakib <nazmuss247@gmail.com>
2025-05-11 00:43:09 +0600

    dtring input added

---
+++ 05_Arrays_Strings_Input/16_User_Input/02_String_Input.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    char firstName[30];
+
+    //Asking user to input
+    printf("Enter your Name here: \n");
+
+//printing values
+scanf("%s", &firstName);
+printf("Your Name is: %s", firstName);
+return 0;
+}
