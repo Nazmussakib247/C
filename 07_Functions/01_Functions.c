83dfe932bb164b91db1e51ba468b8dbee771cc76
Nazmus Sakib <nazmuss247@gmail.com>
2025-06-03 00:10:31 +0600

    functions addded

---
+++ 07_Functions/01_Functions.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void myFunction(){
+    printf("Hello Good people, function called succesfulll\n");
+}
+int main(){
+    myFunction();
+    myFunction(); //called function multiple times
+    return 0;
+}
