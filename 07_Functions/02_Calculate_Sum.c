815def5e953ec505e0ba5dc8d7de11664597991d
Nazmus Sakib <nazmuss247@gmail.com>
2025-06-04 02:09:14 +0600

    calculate sum added

---
+++ 07_Functions/02_Calculate_Sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void calculateSum(){
+    int x = 28383;
+    int y = 38929;
+    int sum = x + y;
+    printf("Your total sum is: %d", sum);
+}
+int main(){
+    calculateSum();
+    return 0;
+}
